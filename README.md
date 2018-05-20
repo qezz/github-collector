b0e78c356a26801b06a5d84cc052632bf46417e9
Sergey Mishin <sergei.a.mishin@gmail.com>
2018-05-20 19:56:41 +0300

    WIP

---
+++ README.md
@@ -0,0 +1,8 @@
+# Collect data about users from github
+
+## How to
+
+1. Receive your personal token: https://github.com/settings/tokens
+   * Creeate new if needed
+   * user
+   * Generate token
--- main.go
+++ main.go
@@ -1,10 +1,28 @@
 package main
 
 import (
-	"fmt"
+	"context"
 	"github.com/google/go-github/github"
+	"golang.org/x/oauth2"
+	"log"
+	"os"
 )
 
 func main() {
-	fmt.Println("Hello world")
+	log.Println("Start")
+
+	ghToken := os.Getenv("GITHUB_TOKEN")
+	if ghToken == "" {
+		log.Fatal("You should privide GITHUB_TOKEN as environment variable.")
+		return
+	}
+
+	ctx := context.Background()
+	ts := oauth2.StaticTokenSource(
+		&oauth2.Token{AccessToken: ghToken},
+	)
+	tc := oauth2.NewClient(ctx, ts)
+
+	_ = github.NewClient(tc)
+
 }
